b87acda9d46374aeb9f6870bde0a2c9ce429b725
zbikma <z.mazadi@gmail.com>
2021-11-19 15:20:16 -0500

    initial commit

---
+++ JokeApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokeApp.Models;
+
+namespace JokeApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokeApp.Models.Joke> Joke { get; set; }
+    }
+}
